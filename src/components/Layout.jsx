f104709c3025bc71e1c96d111ee5d79ea1777b94
BJ-stack-art <bj.angel119@gmail.com>
2021-07-13 22:08:54 +0700

    initialize this directory become repository git

---
+++ src/components/Layout.jsx
@@ -0,0 +1,127 @@
+import { makeStyles } from '@material-ui/core';
+import React from 'react';
+import { useHistory , useLocation } from 'react-router-dom';
+import Drawer from '@material-ui/core/Drawer';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import { AddCircleOutlined, SubjectOutlined } from '@material-ui/icons';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import Avatar from '@material-ui/core/Avatar';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => {
+    return {
+        page: {
+            backgroundColor: '#f9f9f9',
+            width: '100%',
+            padding: theme.spacing(3),
+        },
+        drawer: {
+            width: drawerWidth,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        root: {
+            display: 'flex',
+        },
+        active: {
+            background: '#f4f4f4',
+        },
+        title: {
+            padding: theme.spacing(2),
+        },
+        appbar: {
+            width: `calc(100% - ${drawerWidth}px)`,
+        },
+        toolbar: theme.mixins.toolbar,
+        appname: {
+            flexGrow: 1
+        },
+        avatar: {
+            marginLeft: theme.spacing(2),
+        }
+    }
+})
+
+export default function Layout({ children }) {
+    const classes = useStyles()
+    const history = useHistory()
+    const location = useLocation()
+
+    const menuItems = [
+        {
+            text: 'My Notes',
+            icon: <SubjectOutlined color="secondary" />,
+            paths: '/'
+        },
+        {
+            text: 'Create Notes',
+            icon: <AddCircleOutlined color="secondary" />,
+            paths: '/create'
+        },
+    ]
+    
+    return (
+        <div className={classes.root} >
+            {/* App bar */}
+            <AppBar
+                className={classes.appbar}
+                elevation={0}
+            >
+                <ToolBar>
+                    <Typography className={classes.appname}>
+                        MUI
+                    </Typography>
+                    <Typography>
+                        Abdul Latif
+                    </Typography>
+                    <Avatar src="/latif.jpg" className={classes.avatar} />
+                </ToolBar>
+            </AppBar>
+
+            {/* side drawer */}
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                anchor="left"
+                classes={{ paper: classes.drawerPaper }}
+            >
+                <div>
+                    <Typography variant="h5" className={classes.title} >
+                        Ninja Notes
+                    </Typography>
+                </div>
+
+               {/* LIst links */}
+               <List>
+                   {menuItems.map(item => (
+                       <ListItem
+                        button
+                        key={item.text}
+                        onClick={() => history.push(item.paths)}
+                        className={location.pathname === item.paths ? classes.active : null}
+                       >
+                           <ListItemIcon>{item.icon}</ListItemIcon>
+                           <ListItemText primary={item.text} />
+                       </ListItem>
+                   ))}
+               </List>
+
+                
+            </Drawer>
+            
+            
+
+            <div className={classes.page} >
+                <div className={classes.toolbar} ></div>
+                    {children}
+            </div>
+        </div>
+    )
+}
